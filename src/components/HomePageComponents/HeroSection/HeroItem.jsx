2168f2fcb29e45ef578e9ab775c914a379ecf75f
SoltanMehrshad <soltanmehrshad02@gmail.com>
2022-01-26 20:19:20 +0100

    fixed herosection text height

--- src/components/HomePageComponents/HeroSection/HeroItem.jsx
+++ src/components/HomePageComponents/HeroSection/HeroItem.jsx
@@ -2,15 +2,22 @@ import styles from './HeroItem.module.scss';
 
 const HeroItem = ({ url, title, subtitle, country }) => {
     const height = window.innerHeight;
+    console.log(height);
     return (
-      <div className={`${styles.root} nav-padding pb-5 d-flex flex-column`}>
+      <div
+        style={{ height: height }}
+        className={`${styles.root} nav-padding pb-5 d-flex flex-column`}
+      >
         <img
           style={{ height: height }}
           alt=""
           src={url}
           className={styles.img}
         />
-        <div className="d-flex position-relative z-index-1 flex-column flex-1 text-center container">
+        <div
+          style={{ height: height }}
+          className="d-flex position-relative z-index-1 flex-column flex-1 text-center container"
+        >
           <div className="my-auto pt-hero ">
             <h1 className="h1">{title}</h1>
             <h2 className="h2">{subtitle}</h2>
--- src/components/HomePageComponents/HeroSection/HeroSection.jsx
+++ src/components/HomePageComponents/HeroSection/HeroSection.jsx
@@ -6,41 +6,45 @@ import img3 from '../../../assets/3.jpeg'
 import img4 from '../../../assets/4.jpeg'
 import img5 from '../../../assets/5.jpeg'
 const HeroSection = ({ toggleMenu }) => {
+
+  const height = window.innerHeight;
+
   return (
-    <div className="HeroSection-wrapper">
-      <div className="vh-100 tx-light position-relative HomePage-HeroSection">
-        {/* <div onMouseOver={() => toggleMenu(true)} className="hover-column"></div> */}
-        <HeroItem
-          url={img1}
-          title="CABINET BONAPARTE"
-          subtitle="L'immobilier haute couture"
-          country="PARIS / LYON / MARSEILLE"
-        />
-        <HeroItem
-          url={img2}
-          title="CABINET BONAPARTE"
-          subtitle="Oser voir grand"
-          country="PARIS / LYON / MARSEILLE"
-        />
-        <HeroItem
-          url={img3}
-          title="CABINET BONAPARTE"
-          subtitle="Construire les rêves"
-          country="PARIS / LYON / MARSEILLE"
-        />
-        <HeroItem
-          url={img4}
-          title="CABINET BONAPARTE"
-          subtitle="Investire dans l'intemporel"
-          country="PARIS / LYON / MARSEILLE"
-        />
-        <HeroItem
-          url={img5}
-          title="CABINET BONAPARTE"
-          subtitle="Sublimer avec caractère"
-          country="PARIS / LYON / MARSEILLE"
-        />
-      </div>
+    <div
+      style={{ height: height }}
+      className=" tx-light position-relative HomePage-HeroSection"
+    >
+      {/* <div onMouseOver={() => toggleMenu(true)} className="hover-column"></div> */}
+      <HeroItem
+        url={img1}
+        title="CABINET BONAPARTE"
+        subtitle="L'immobilier haute couture"
+        country="PARIS / LYON / MARSEILLE"
+      />
+      <HeroItem
+        url={img2}
+        title="CABINET BONAPARTE"
+        subtitle="Oser voir grand"
+        country="PARIS / LYON / MARSEILLE"
+      />
+      <HeroItem
+        url={img3}
+        title="CABINET BONAPARTE"
+        subtitle="Construire les rêves"
+        country="PARIS / LYON / MARSEILLE"
+      />
+      <HeroItem
+        url={img4}
+        title="CABINET BONAPARTE"
+        subtitle="Investire dans l'intemporel"
+        country="PARIS / LYON / MARSEILLE"
+      />
+      <HeroItem
+        url={img5}
+        title="CABINET BONAPARTE"
+        subtitle="Sublimer avec caractère"
+        country="PARIS / LYON / MARSEILLE"
+      />
     </div>
   );
 };
