5efce33931ca32270ef4be362a9e435c1c32cb0c
SoltanMehrshad <soltanmehrshad02@gmail.com>
2022-01-27 15:09:31 +0100

    Pdf width on mobile screen and register page's header fixed

--- src/components/PDF/pdfPages.jsx
+++ src/components/PDF/pdfPages.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 import { Document, Page, pdfjs } from "react-pdf";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 export default function AllPages(props) {
   const [numPages, setNumPages] = useState(null);
-
+  const [width, setWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    window.addEventListener("resize", () => setWidth(window.innerWidth));
+    console.log(width);
+  }, []);
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
   }
@@ -18,7 +22,11 @@ export default function AllPages(props) {
       onLoadSuccess={onDocumentLoadSuccess}
     >
       {Array.from(new Array(numPages), (el, index) => (
-        <Page scale={1.2} key={`page_${index + 1}`} pageNumber={index + 1} />
+        <Page
+          scale={width > 760 ? 1.2 : 0.4}
+          key={`page_${index + 1}`}
+          pageNumber={index + 1}
+        />
       ))}
     </Document>
   );
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -13,58 +13,63 @@ const RegisterPage = () => {
     backgroundColor: "rgb(255, 255, 255, 0.9)",
   };
   return (
-    <div
-      style={{ backgroundImage: `url(${background})`, backgroundSize: "cover" }}
-    >
-      <div className="d-flex min-vh-100 nav-padding pb-5 mt-md-3 tx-dark bg-sm-light">
-        <HeaderV2
-          isMenuOpen={isMenuOpen}
-          setIsMenuOpen={(e) => setIsMenuOpen(e)}
-        />
-        <form
-          className="container p-lg-5 p-md-4 m-auto d-flex flex-column gap-md-5 gap-md-4 gap-3 "
-          style={style}
-        >
-          <h2 className="text-center h2 my-0">Inscription</h2>
-          <div>
-            <h5 className="mt-0 mb-2 h5">Civilité</h5>
-            <div className="d-flex gap-3">
-              <BoxField label="M." value="Mr" type="radio" name="gender" />
-              <BoxField label="Mme." value="Mrs" type="radio" name="gender" />
-              <BoxField label="Mlle." value="Ms" type="radio" name="gender" />
+    <>
+      <HeaderV2
+        isMenuOpen={isMenuOpen}
+        setIsMenuOpen={(e) => setIsMenuOpen(e)}
+      />
+      <div
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="d-flex min-vh-100 nav-padding pb-5 mt-md-3 tx-dark bg-sm-light">
+          <form
+            className="container p-lg-5 p-md-4 m-auto d-flex flex-column gap-md-5 gap-md-4 gap-3 "
+            style={style}
+          >
+            <h2 className="text-center h2 my-0">Inscription</h2>
+            <div>
+              <h5 className="mt-0 mb-2 h5">Civilité</h5>
+              <div className="d-flex gap-3">
+                <BoxField label="M." value="Mr" type="radio" name="gender" />
+                <BoxField label="Mme." value="Mrs" type="radio" name="gender" />
+                <BoxField label="Mlle." value="Ms" type="radio" name="gender" />
+              </div>
             </div>
-          </div>
-          <div className="d-grid col-md-2 gap-inherit">
-            <TextField label="Nom" />
-            <TextField label="Prenom" />
-          </div>
-          <TextField label="Numéro de telephone" />
-          <TextField label="Email" />
-          <div className="d-grid col-md-2 gap-inherit">
-            <TextField label="Mot de passe" />
-            <TextField label="Mot de passe - confirmation" />
-          </div>
-          <BoxField
-            label={
-              <>
-                {" "}
-                J’accepte que Cabinet Bonaparte m’envoie par e-mail ses <br />{" "}
-                actualités et ses offres concernant ses autres services
-              </>
-            }
-          />
-          <div className="d-flex gap-md-4 gap-3 align-items-center">
-            <button className="btn-form">CONNEXION</button>
-            <span>
-              Already have an account,{" "}
-              <Link to="/login" className="btn-text">
-                Login
-              </Link>
-            </span>
-          </div>
-        </form>
+            <div className="d-grid col-md-2 gap-inherit">
+              <TextField label="Nom" />
+              <TextField label="Prenom" />
+            </div>
+            <TextField label="Numéro de telephone" />
+            <TextField label="Email" />
+            <div className="d-grid col-md-2 gap-inherit">
+              <TextField label="Mot de passe" />
+              <TextField label="Mot de passe - confirmation" />
+            </div>
+            <BoxField
+              label={
+                <>
+                  {" "}
+                  J’accepte que Cabinet Bonaparte m’envoie par e-mail ses <br />{" "}
+                  actualités et ses offres concernant ses autres services
+                </>
+              }
+            />
+            <div className="d-flex gap-md-4 gap-3 align-items-center">
+              <button className="btn-form">CONNEXION</button>
+              <span>
+                Already have an account,{" "}
+                <Link to="/login" className="btn-text">
+                  Login
+                </Link>
+              </span>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
